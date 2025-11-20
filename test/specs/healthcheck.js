ef41389fbe39ef62222b35db5af5231abf861fdf
sdingankar99 <sdingankar@equalexperts.com>
2025-11-20 15:36:48 +0000

    ULTIP-3492 updated to fix lint errors
    updated to fix lint error

--- test/specs/healthcheck.js
+++ test/specs/healthcheck.js
@@ -2,24 +2,18 @@ import { expect } from 'chai'
 import { describe, it } from 'mocha'
 import { apiHealthCheck } from '../helpers/apicall.js'
 import {
-  keeperDataBridgeEphemeralUrl,
-  keeperDataAPIEphemeralUrl,
   keeperDataBridgeUrl,
   keeperDataAPIUrl
 } from '../helpers/apiEndpoints.js'
 
 describe('API Health Check', () => {
   it('should return status 200 for ls-keeper-data-bridge-backend health check endpoint', async () => {
-    const response = await apiHealthCheck(keeperDataBridgeEphemeralUrl)
-    console.log('Response status:', response.status)
-    console.log('Response data:', response.data)
+    const response = await apiHealthCheck(keeperDataBridgeUrl)
     expect(response.status).to.equal(200)
   })
 
   it('should return status 200 for ls-keeper-data-api health check endpoint', async () => {
-    const response = await apiHealthCheck(keeperDataAPIEphemeralUrl)
-    console.log('Response status:', response.status)
-    console.log('Response data:', response.data)
+    const response = await apiHealthCheck(keeperDataAPIUrl)
     expect(response.status).to.equal(200)
   })
 })
