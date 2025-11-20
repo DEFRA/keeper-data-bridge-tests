dbb7cb6a3f1eab3f8793024776663afa7908470c
sdingankar99 <sdingankar@equalexperts.com>
2025-11-20 15:22:46 +0000

    ULLITP-3492-fixing code style issues.
    Fixing code style issues found during PR

--- test/helpers/apicall.js
+++ test/helpers/apicall.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 import { apiKey, healthEndpoint } from "./apiEndpoints.js";
- 
-
 
 export async function apiHealthCheck(url) {
     const apiHealthCheckResponse = await axios.get(url + healthEndpoint, {
         headers: {
-            "x-api-key": apiKey
-        }
+            "x-api-key": apiKey,
+        },
     });
     return apiHealthCheckResponse;
 }
--- test/specs/healthcheck.js
+++ test/specs/healthcheck.js
@@ -1,23 +1,25 @@
-import { expect } from "chai"; 
+import { expect } from "chai";
 import { describe, it } from "mocha";
 import { apiHealthCheck } from "../helpers/apicall.js";
-import { keeperDataBridgeEphemeralUrl, keeperDataAPIEphemeralUrl, keeperDataBridgeUrl, keeperDataAPIUrl } from "../helpers/apiEndpoints.js";
-
-
-
+import {
+  keeperDataBridgeEphemeralUrl,
+  keeperDataAPIEphemeralUrl,
+  keeperDataBridgeUrl,
+  keeperDataAPIUrl,
+} from "../helpers/apiEndpoints.js";
 
 describe("API Health Check", () => {
-    it("should return status 200 for ls-keeper-data-bridge-backend health check endpoint", async () => {
-        const response = await apiHealthCheck(keeperDataBridgeEphemeralUrl);
-        console.log("Response status:", response.status);
-        console.log("Response data:", response.data);
-        expect(response.status).to.equal(200); 
-    });
-    
-    it("should return status 200 for ls-keeper-data-api health check endpoint", async () => {
-        const response = await apiHealthCheck(keeperDataAPIEphemeralUrl);
-        console.log("Response status:", response.status);
-        console.log("Response data:", response.data);
-        expect(response.status).to.equal(200); 
-    });
+  it("should return status 200 for ls-keeper-data-bridge-backend health check endpoint", async () => {
+    const response = await apiHealthCheck(keeperDataBridgeEphemeralUrl);
+    console.log("Response status:", response.status);
+    console.log("Response data:", response.data);
+    expect(response.status).to.equal(200);
+  });
+
+  it("should return status 200 for ls-keeper-data-api health check endpoint", async () => {
+    const response = await apiHealthCheck(keeperDataAPIEphemeralUrl);
+    console.log("Response status:", response.status);
+    console.log("Response data:", response.data);
+    expect(response.status).to.equal(200);
+  });
 });
